d3c0360b136b02ea0321bf5405f17408acaabc39
FarhanMU <obet529nf@Gmail.com>
2024-06-12 17:04:07 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { Client } = require("basic-ftp");
 const fs = require("fs");
 const app = express();
-const port = 3003;
+const port = 3000;
 
 app.get("/download", async (req, res) => {
   const client = new Client();

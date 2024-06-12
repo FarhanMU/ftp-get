6370898fe2888dbc91028524f49e54e4eed9f4ce
FarhanMU <obet529nf@Gmail.com>
2024-06-12 17:03:21 +0700

    update

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-const PromiseFtp = require("promise-ftp");
+const express = require("express");
+const { Client } = require("basic-ftp");
 const fs = require("fs");
+const app = express();
+const port = 3003;
 
-const ftp = new PromiseFtp();
-const config = {
-  host: "ftp.jevidrive.com",
-  user: "ftpuser",
-  password: "1sampai8",
-};
+app.get("/download", async (req, res) => {
+  const client = new Client();
+  client.ftp.verbose = true; // Aktifkan mode verbose untuk melihat log detail
 
-ftp
-  .connect(config)
-  .then(() => {
-    return ftp.get("/srv/files/ftp/diagnostics/data.txt"); // Pastikan path ini benar
-  })
-  .then((stream) => {
-    return new Promise((resolve, reject) => {
-      stream.once("close", resolve);
-      stream.once("error", reject);
-      stream.pipe(fs.createWriteStream("data.txt"));
+  try {
+    await client.access({
+      host: "milan.id.domainesia.com",
+      user: "stikerid",
+      password: "I1Mlj0;k71RKv;",
+      secure: false, // Gunakan `true` untuk koneksi FTPS (FTP Secure)
     });
-  })
-  .then(() => {
-    console.log("File downloaded successfully!");
-    return ftp.end();
-  })
-  .catch((err) => {
-    console.error("Error:", err.message);
-  });
+    const server_file = "testupload.eunoiaid.com/testUpload/data.txt";
+    const local_file = "./download/data.txt"; // File akan disimpan di folder lokal proyek
+    await client.download(fs.createWriteStream(local_file), server_file);
+    res.send("File successfully downloaded!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to download file.");
+  }
+
+  client.close();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
